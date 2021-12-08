ab63a42e05dc38031a53f5cb0d0b04595d30124a
Rogowski Mirosław <mirek.rogowski@hotmail.com>
2021-12-08 13:10:14 +0100

    Update bazaszkoly.py
    class Students

--- bazaszkoly.py
+++ bazaszkoly.py
@@ -10,12 +10,29 @@ class Classroom:
         self.educators = []
 
 
+def input_class_name():
+    input_class_name = input("Podaj nazwę klasy: ")
+    input_class_name_list = []
+    while input_class_name:
+        if input_class_name not in data_school:
+            data_school[input_class_name ] = Classroom(input_class_name)
+        input_class_name_list.append(input_class_name)
+        input_class_name = input("Podaj nazwę klasy: ")
+    return input_class_name_list
+
+
 class Student:
 
     def __init__(self, name):
         print("create student\n")
         self.name = name
         self.class_name = ""
+
+    def get_classes(self):
+        self.class_name = input("Podaj klasę: ")
+        if self.class_name not in data_school:
+            data_school[self.class_name] = Classroom(self.class_name)
+        data_school[self.class_name].students.append(self.name)
 
 
 class Teacher:
@@ -35,6 +52,17 @@ class Tutor:
         self.class_names = []
 
 
-
+while True:
+    user_type = input(("Podaj typ użytkownika: "))
+    if user_type == "koniec" or user_type == "k":
+        print("Koniec programu. ")
+        break
+    user_name = input("Wprowadź imie i nazwisko: ")
+    if user_type == "uczen" or user_type == "u":
+        person = Student(user_name)
+        person.get_classes()
+    else:
+        print("Nieprawidłowy typ uzytkownika: ")
+        break
 
 
